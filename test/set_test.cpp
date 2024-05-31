6471c49855dad01e3f2abefe4d40c19a1107613f
Erick Velez <erickvelez7@gmail.com>
2024-05-30 17:35:56 -0700

    set_test: add remove test

--- test/set_test.cpp
+++ test/set_test.cpp
@@ -19,3 +19,14 @@ TEST(SetTest, Clear)
   ASSERT_NO_THROW(s.clear());
   EXPECT_TRUE(s.empty());
 }
+
+TEST(SetTest, Remove)
+{
+  ev::set<int> s;
+  s.insert(10);
+  s.insert(3);
+  s.insert(20);
+  s.insert(30);
+  s.insert(25);
+  EXPECT_NO_THROW(s.remove(25));
+}
